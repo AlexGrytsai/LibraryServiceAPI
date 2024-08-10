97a2d43d1425d7b4e56d2af2d3f09803829199df
Alex Grytsai <a.grytsai1987@gmail.com>
2024-08-10 15:25:09 +0300

    added url for media files

--- LibraryServiceAPI/settings.py
+++ LibraryServiceAPI/settings.py
@@ -132,6 +132,9 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+MEDIA_URL = "/mediafiles/"
+MEDIA_ROOT = BASE_DIR / "mediafiles"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
--- LibraryServiceAPI/urls.py
+++ LibraryServiceAPI/urls.py
@@ -14,7 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib.auth.views import LogoutView
 from django.urls import path
 from debug_toolbar.toolbar import debug_toolbar_urls
@@ -39,4 +40,6 @@ urlpatterns = [
         "api/v1/token/verify/", TokenVerifyView.as_view(), name="token_verify"
     ),
     path("api/v1/token/logout/", LogoutView.as_view(), name="token_logout"),
-] + debug_toolbar_urls()
+] + debug_toolbar_urls() + static(
+    settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+)
