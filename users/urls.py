2b4a662a8b4d22b12662562cbc7fb274c8b1b50a
Alex Grytsai <a.grytsai1987@gmail.com>
2024-08-10 16:34:19 +0300

    Created ManageUserView and UserPasswordUpdateView. Added urls for views to users.urls

--- users/urls.py
+++ users/urls.py
@@ -1,3 +1,9 @@
-urlpatterns = []
+from django.urls import path
+
+from users.views import ManageUserView, UserPasswordUpdateView
+urlpatterns = [
+    path("me/", ManageUserView.as_view(), name="me"),
+    path("me/password/", UserPasswordUpdateView.as_view(), name="password"),
+]
 
 app_name = "users"
--- users/views.py
+++ users/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+from typing import Type
+
+from django.db.models import QuerySet
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.serializers import Serializer
+
+from users.models import User
+from users.serializers import UserManageSerializer, UserUpdateSerializer, \
+    UserPasswordUpdateSerializer
+
+
+class ManageUserView(generics.RetrieveUpdateDestroyAPIView):
+    """
+    API endpoint that allows users to be viewed or edited without a password.
+    """
+
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self) -> QuerySet:
+        user = self.request.user
+
+        return (
+            User.objects.all()
+            .filter(id=user.id)
+        )
+
+    def get_object(self) -> User:
+        return self.request.user
+
+    def get_serializer_class(self) -> Type[Serializer]:
+        if self.request.method == "GET":
+            return UserManageSerializer
+        return UserUpdateSerializer
+
+
+class UserPasswordUpdateView(generics.UpdateAPIView):
+    """
+    API endpoint that allows users update their password.
+    """
+
+    serializer_class = UserPasswordUpdateSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_object(self) -> User:
+        return self.request.user
 
 # Create your views here.
