fb1daa3fa5f2a37f960442f890b4638941322ccc
Alex Grytsai <a.grytsai1987@gmail.com>
2024-08-10 15:32:05 +0300

    fixed PEP in maine urls

--- LibraryServiceAPI/urls.py
+++ LibraryServiceAPI/urls.py
@@ -53,7 +53,9 @@ urlpatterns = (
             SpectacularSwaggerView.as_view(url_name="schema"),
             name="swagger-ui",
         ),
-        path("api/v1/customer/", include("customer.urls", namespace="customer")),
+        path(
+            "api/v1/customer/", include("customer.urls", namespace="customer")
+        ),
     ]
     + debug_toolbar_urls()
     + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
