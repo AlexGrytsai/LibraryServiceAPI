da7dec74f92bf51a67be1a9a8dcd4925deffc86c
Alex Grytsai <a.grytsai1987@gmail.com>
2024-08-11 10:33:23 +0300

    created a BookView (CRUD for books) and added url to books.urls

--- books/urls.py
+++ books/urls.py
@@ -1,3 +1,13 @@
-urlpatterns = []
+from django.urls import include, path
+from rest_framework import routers
+
+from books.views import BookView
+
+router = routers.DefaultRouter()
+router.register("books", BookView)
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
 
 app_name = "books"
--- books/views.py
+++ books/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import AllowAny, IsAdminUser
 
-# Create your views here.
+from books.models import Book
+from books.serializers import BookSerializer, BookListSerializer
+
+
+class BookView(viewsets.ModelViewSet):
+
+    queryset = Book.objects.all()
+
+    def get_permissions(self):
+        if self.request.method in ["POST", "PUT", "PATCH", "DELETE"]:
+            return (IsAdminUser(),)
+        return (AllowAny(),)
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return BookListSerializer
+        return BookSerializer
+
+    def get_queryset(self):
+        if self.action == "retrieve":
+            return Book.objects.filter(id=self.kwargs["pk"])
+        return super(BookView, self).get_queryset()
